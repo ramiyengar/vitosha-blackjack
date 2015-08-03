bff3202c8b941db212e0721b208f081dc7d93271
Todor Balabanov <todor.balabanov@gmail.com>
2015-08-03 18:40:18 +0300

    Android user interface was implemented.

--- src/eu/veldsoft/vitosha/blackjack/GameActivity.java
+++ src/eu/veldsoft/vitosha/blackjack/GameActivity.java
@@ -1,8 +1,10 @@
 package eu.veldsoft.vitosha.blackjack;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 
 public class GameActivity extends Activity {
@@ -12,4 +14,32 @@ public class GameActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_game);
 	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.menu_game, menu);
+		return super.onCreateOptionsMenu(menu);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.credit100:
+			//TODO Add 100 to player's credit.
+			startActivity(new Intent(this, InnerBannerActivity.class));
+			break;
+		case R.id.update_player:
+			startActivity(new Intent(this, UpdatePlayerActivity.class));
+			break;
+		case R.id.help:
+			startActivity(new Intent(this, HelpActivity.class));
+			break;
+		case R.id.about:
+			startActivity(new Intent(this, AboutActivity.class));
+			break;
+		}
+		return true;
+	}
+
 }
